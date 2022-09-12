4ba1100d147e5f3c8305a5a58cf087de34e7413e
Natania Deandra <nataniadeandra@gmail.com>
2022-09-12 19:13:23 +0700

    Update several files

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    return render(request, "katalog.html", context)
+
+data_catalog_item = CatalogItem.objects.all()
+context = {
+    'list_barang': data_catalog_item,
+    'nama': 'Natania Deandra',
+    'npm': 2106633090,
+}
