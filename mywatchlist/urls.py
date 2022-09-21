0f2ab58e0ba165b6242e13d48f42d71a5d4790cb
Natania Deandra <nataniadeandra@gmail.com>
2022-09-21 14:04:30 +0700

    fix deployment

--- mywatchlist/urls.py
+++ mywatchlist/urls.py
@@ -6,9 +6,9 @@ from mywatchlist.views import show_pesan
 
 # app_name = 'mywatchlist'
 
-# urlpatterns = [
-#     path('', show_pesan, name='show_pesan'),
-#     path('html/', show_html, name='show_html'),
-#     path('xml/', show_xml, name='show_xml'),
-#     path('json/', show_json, name='show_json'),
-# ]
+urlpatterns = [
+    path('', show_pesan, name='show_pesan'),
+    path('html/', show_html, name='show_html'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+]
